6df1f71b09adbd58ab27f32a2b8dc08541a6c662
Cédric Cabessa <ccabessa@genymobile.com>
2014-11-24 15:13:18 +0100

    nb comment + other changes

--- gm_pr_app/models.py
+++ gm_pr_app/models.py
@@ -1,9 +1,11 @@
 class Pr:
-    def __init__(self, url = "", title = "", updated_at = "", user = "", repo = ""):
+    def __init__(self, url = "", title = "", updated_at = "", user = "",
+                 repo = "", nbreview = 0):
         self.url = url
         self.title = title
         self.updated_at = updated_at
         self.user = user
         self.repo = repo
+        self.nbreview = nbreview
 
 
--- gm_pr_app/views.py
+++ gm_pr_app/views.py
@@ -9,6 +9,14 @@ import urllib.request
 import json
 from multiprocessing import pool
 
+def get_json(url) :
+    response = urllib.request.urlopen(url)
+    charset = response.info().get_content_charset()
+    if charset == None:
+        charset = 'utf-8'
+    string = response.read().decode(charset)
+    return json.loads(string)
+
 
 def fetch_data(project_name):
     pr_list = []
@@ -18,31 +26,31 @@ def fetch_data(project_name):
     url = "%s/repos/%s/%s/pulls" % (settings.TOP_LEVEL_URL,
                                     settings.ORG,
                                     project_name)
-    response = urllib.request.urlopen(url)
-    charset = response.info().get_content_charset()
-    if charset == None:
-        charset = 'utf-8'
-    string = response.read().decode(charset)
-    jdata = json.loads(string)
+    jdata = get_json(url)
     if len(jdata) == 0:
         return
     for jpr in jdata:
         if jpr['state'] == 'open':
+            review_json = get_json(jpr['comments_url'])
+
             pr = models.Pr(url = jpr['html_url'],
                            title = jpr['title'],
                            updated_at = jpr['updated_at'],
                            user = jpr['user']['login'],
-                           repo = jpr['base']['repo']['full_name'])
+                           repo = jpr['base']['repo']['full_name'],
+                           nbreview = len(review_json))
             pr_list.append(pr)
 
     sorted(pr_list, key=lambda pr: pr.updated_at)
 
     return project
 
+import time
 
 def index(request):
+    before = time.time()
     project_list = []
-    p = pool.Pool(10)
+    p = pool.Pool(20)
     context = { "project_list" : project_list }
 
     workers = []
@@ -55,4 +63,6 @@ def index(request):
     for worker in workers:
         project_list.append(worker.get())
 
+    after = time.time()
+    print(after - before)
     return render(request, 'pr.html', context)
