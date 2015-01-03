e504442fe56a935fae4fc3394ab36f38c92f4806
Cédric Cabessa <ced@ryick.net>
2015-01-03 21:56:31 +0100

    auth

--- bot/views.py
+++ bot/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 
-from bot import tasks
+from bot import tasks, slackAuth
 
+@slackAuth.isFromSlack
 def index(request):
     tasks.slack.delay()
     return HttpResponse("One moment, Octocat is considering your request\n")
