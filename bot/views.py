c5dabc7d7c1762959060216b5df59586bad96a5d
Cédric Cabessa <ced@ryick.net>
2015-08-23 21:12:31 +0200

    fix bot with parallel code
    I did not succeed to have a background task (delay) that can wait for other
    (parallel) subtask to complete. It seems that calling get() inside a
    background task is forbidden.
    For now, the slack bot will be synchroneous, this mean the user will not see
    a message until the processing is complete. This also means that we keep the
    connexion between slack and us open during all this time (~10s) but
    apparently slack do not cut it.

--- bot/views.py
+++ bot/views.py
@@ -21,12 +21,12 @@ from bot import tasks, slackauth
 def index(request):
     project, repos = proj_repo.proj_repo(request)
     if repos != None:
-        tasks.slack.delay(settings.TOP_LEVEL_URL,
-                          settings.ORG,
-                          "%s?project=%s" % (settings.WEB_URL, project),
-                          repos,
-                          settings.SLACK_URL,
-                          "#%s" % project)
-        return HttpResponse("One moment, Octocat is considering your request\n")
+        tasks.slack(settings.TOP_LEVEL_URL,
+                    settings.ORG,
+                    "%s?project=%s" % (settings.WEB_URL, project),
+                    repos,
+                    settings.SLACK_URL,
+                    "#%s" % project)
+        return HttpResponse("Octocat thank you for your business\n")
     else:
         return HttpResponse("No projects found\n", status=404)
